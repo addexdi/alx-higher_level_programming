1afd50eb42521e848950fdd91cf495c259d79c89
[Adam Ibrahim] <onelordad@gmail.com>
2022-06-07 01:17:07 +0100

    elements

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
-element_at = __import__('1-element_at').element_at
+def element_at(my_list, idx):
+    if idx < 0:
+        return None
+    if idx >= len(my_list):
+        return None
+    return(my_list[idx])
 
-my_list = [1, 2, 3, 4, 5]
-idx = 3
-print("Element at index {:d} is {}".format(idx, element_at(my_list, idx)))
