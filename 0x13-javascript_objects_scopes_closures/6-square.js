d6175d3328526dd66d59dd56c2a7cb4b8e101a97
[Adam Ibrahim] <onelordad@gmail.com>
2022-09-09 23:09:45 +0100

    javascript objects

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+const Rectangle = require('./5-square');
+module.exports = class Square extends Rectangle {
+  constructor (size) {
+    super(size, size);
+  }
+
+  charPrint (c) {
+    if (c === undefined) {
+      this.print();
+    } else {
+      for (let i = 0; i < this.width; i++) {
+        console.log(c.repeat(this.height));
+      }
+    }
+  }
+};
